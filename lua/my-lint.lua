6f23a5c767020cd2ec29ca956b30507f83632b12
Szwendacz99 <maciejlebiest@proton.me>
2022-11-02 11:53:05 +0100

    disable python module docstring linting

--- lua/my-lint.lua
+++ lua/my-lint.lua
@@ -1,18 +1,24 @@
 require('lint').linters_by_ft = {
-  python = {'flake8', 'pylint', 'pycodestyle', 'pydocstyle',},
-  php = {'phpcs'},
-  markdown = {'markdownlint'}
+    python = { 'flake8', 'pylint', 'pycodestyle', },
+    php = { 'phpcs' },
+    markdown = { 'markdownlint' }
+}
+
+local pylint = require('lint.linters.pylint')
+pylint.args = {
+    '-f',
+    'json',
+    '--disable=C0114', -- disable missing module docstring info
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
+    callback = function()
+        require("lint").try_lint()
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
+    callback = function()
+        require("lint").try_lint()
+    end,
 })
-
