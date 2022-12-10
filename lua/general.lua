be1be8de9c1abd4848a10adf0bfee5338078b28f
Szwendacz99 <maciejlebiest@proton.me>
2022-12-10 18:36:30 +0100

    colorscheme set to gruvbox

--- lua/general.lua
+++ lua/general.lua
@@ -342,7 +342,7 @@ require("gruvbox").setup({
     invert_tabline = false,
     invert_intend_guides = false,
     inverse = true, -- invert background for search, diffs, statuslines and errors
-    contrast = "hard", -- can be "hard", "soft" or empty string
+    contrast = "", -- can be "hard", "soft" or empty string
     palette_overrides = {},
     overrides = {},
     dim_inactive = true, -- default=false
