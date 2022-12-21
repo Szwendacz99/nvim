0149f2023b12747e08a00069f6bc59f250373f16
Szwendacz99 <maciejlebiest@proton.me>
2022-12-21 21:29:31 +0100

    move lsp config after treesitter to make lsp stuff work properly

--- lua/initializer.lua
+++ lua/initializer.lua
@@ -1,7 +1,6 @@
 -- load all modules
 require("packer-setup")
 require("general")
-require("my-lspconfig")
 require("my-lint")
 require("conf-hlslens")
 require("conf-gitsigns")
@@ -12,3 +11,4 @@ require("conf-nvim-tree")
 require("conf-bufferline")
 require("conf-themes")
 require("conf-treesitter")
+require("my-lspconfig") -- need to be after treesitter initialization
