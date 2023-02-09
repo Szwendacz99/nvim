e319cb3c2f8f7c524ab7f474c7ffe3c1543c3470
Maciej Lebiest <>
2023-02-09 19:46:26 +0100

    add few UI plugins, shiny

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -15,14 +15,14 @@ require("lazy").setup({
     { 'mfussenegger/nvim-lint' },
     { 'onsails/lspkind.nvim' },
     { 'folke/trouble.nvim',
-    keys = {
-        { "<leader>xx", "<cmd>TroubleToggle<cr>" },
-        { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
-        { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
-        { "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
-        { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
-        { "gR", "<cmd>TroubleToggle lsp_references<cr>" }
-}},
+        keys = {
+            { "<leader>xx", "<cmd>TroubleToggle<cr>" },
+            { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
+            { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
+            { "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
+            { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
+            { "gR",         "<cmd>TroubleToggle lsp_references<cr>" }
+        } },
 
     -- various plugins
     { 'lewis6991/gitsigns.nvim' },
@@ -40,11 +40,56 @@ require("lazy").setup({
     { 'vmware-archive/salt-vim' },
     { 'stephpy/vim-yaml' },
     { 'lukas-reineke/indent-blankline.nvim' },
+    { 'rcarriga/nvim-notify' },
+    { 'MunifTanjim/nui.nvim' },
+    { 'folke/noice.nvim',
+        config = {
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true,
+                },
+            },
+            -- you can enable a preset for easier configuration
+            presets = {
+                bottom_search = true, -- use a classic bottom cmdline for search
+                command_palette = true, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false, -- add a border to hover docs and signature help
+            },
+            hover = {
+                enabled = true,
+                view = nil, -- when nil, use defaults from documentation
+                ---@type NoiceViewOptions
+                opts = {}, -- merged with defaults from documentation
+            },
+            signature = {
+                enabled = false,
+                auto_open = {
+                    enabled = true,
+                    trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+                    luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+                    throttle = 50, -- Debounce lsp signature help request by 50ms
+                },
+                view = nil, -- when nil, use defaults from documentation
+                ---@type NoiceViewOptions
+                opts = {}, -- merged with defaults from documentation
+            },
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify", } },
 
     { 'nvim-lualine/lualine.nvim' },
     { 'RRethy/vim-illuminate' },
     { 'sheerun/vim-polyglot' },
-    { 'ray-x/lsp_signature.nvim' },
+    --{ 'ray-x/lsp_signature.nvim' },
 
     -- themes
     { 'olimorris/onedarkpro.nvim' },
@@ -101,7 +146,7 @@ require("lazy").setup({
             -- install missing plugins on startup. This doesn't increase startup time.
             missing = true,
             -- try to load one of these colorschemes when starting an installation during startup
-            colorscheme = { },
+            colorscheme = {},
         },
         ui = {
             -- a number <1 is a percentage., >1 is a fixed size
