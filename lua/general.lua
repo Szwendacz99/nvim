f1f46c229ac6ba4aa430b148dd6b04e6075e800b
Maciej Lebiest <szwendacz@proton.me>
2023-02-11 18:36:50 +0100

    finish migrating all to lua

--- lua/general.lua
+++ lua/general.lua
@@ -12,3 +12,33 @@ codewindow.setup({
     z_index = 1, -- The z-index the floating window will be on
 })
 codewindow.apply_default_keybinds()
+
+local function open_nvim_tree()
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+-- Functional wrapper for mapping custom keybindings
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+----------------------
+-- general setup start
+----------------------
+
+-- open nvim tree on start
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+-- tab lines in normal and visual mode
+map("n", "<Tab>", ">>")
+map("n", "<S-Tab>", "<<")
+map("i", "<S-Tab>", "<Esc><<i")
+map("n", "<Tab>", ">>")
+map("v", "<Tab>", ">gv")
+map("v", "<S-Tab>", "<gv")
