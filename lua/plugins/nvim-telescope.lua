3b1466b4b12f5d7fbc9c1bf1afc05f3eeb4ada73
Maciej Lebiest <szwendacz@proton.me>
2024-01-11 21:45:14 +0100

    make Telescope grep hidden files

--- lua/plugins/nvim-telescope.lua
+++ lua/plugins/nvim-telescope.lua
@@ -5,5 +5,43 @@ return {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
+        require('telescope').setup {
+            defaults = {
+                vimgrep_arguments = {
+                    'rg',
+                    '--color=never',
+                    '--no-heading',
+                    '--with-filename',
+                    '--line-number',
+                    '--column',
+                    '--smart-case',
+                    '--no-ignore',
+                    '--hidden',
+                    '--glob',
+                    '!{**/.git/*,**/node_modules/*,**/package-lock.json,**/yarn.lock}',
+                },
+            } }
+    end,
+    opts = {
+        --defaults = {
+        --vimgrep_arguments = {
+        --'rg',
+        --'--color=never',
+        --'--no-heading',
+        --'--with-filename',
+        --'--line-number',
+        --'--column',
+        --'--smart-case',
+        --'--no-ignore',
+        ----'--hidden',
+        --}
+        --},
+        pickers = {
+            live_grep = {
+                additional_args = function(opts)
+                    return { "--hidden" }
+                end
+            },
+        },
+    }
 }
