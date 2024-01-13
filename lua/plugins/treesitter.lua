40d2e7198c692cb29085fb20a161b30d1b26a736
Maciej Lebiest <szwendacz@proton.me>
2024-01-13 21:09:21 +0100

    more treesitter conf adjustments for nvim-in-container workflow

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return function()
     require 'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all"
-        ensure_installed = "all",
+        ensure_installed = "",
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = true,
