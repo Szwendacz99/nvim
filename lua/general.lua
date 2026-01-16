a77dddad5dbb6bf89ad1b1b2f495a78106c5097c
Szwendacz <admin@maciej.cloud>
2026-01-16 11:12:32 +0000

    disable folding

--- lua/general.lua
+++ lua/general.lua
@@ -61,8 +61,9 @@ vim.api.nvim_create_autocmd('FileType', {
     },
     callback = function() vim.treesitter.start() end,
 })
-vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.wo[0][0].foldmethod = 'expr'
+
+--vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+--vim.wo[0][0].foldmethod = 'expr'
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 
 -- open nvim tree on start
